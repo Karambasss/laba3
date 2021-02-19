7a131846d636c4031342b519c7f0d07781585fab
michele <m.papa.09@mail.ru>
2021-02-19 23:05:57 +0200

    WE HAVE SOLVED THAT PROBLEM

--- backup.sh
+++ backup.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
 echo Hello, JuiceWrld
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 echo My name is Michael, and who are you????????????
-a = 5
-=======
->>>>>>> parent of ffa1b90... new commit from linux console
-=======
->>>>>>> parent of ffa1b90... new commit from linux console
+
