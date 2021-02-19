755d5bb8e50429557476cfa39b2da56ae1293ed1
michele <m.papa.09@mail.ru>
2021-02-19 22:40:42 +0200

    initional commit

--- backup.sh
+++ backup.sh
@@ -2,7 +2,10 @@
 
 echo Hello, JuiceWrld
 <<<<<<< HEAD
+<<<<<<< HEAD
 echo My name is Michael, and who are you????????????
 a = 5
+=======
+>>>>>>> parent of ffa1b90... new commit from linux console
 =======
 >>>>>>> parent of ffa1b90... new commit from linux console
